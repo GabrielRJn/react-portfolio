d56a1de76b05ae838594c935d777ab3a51f114e5
GabrielRJn <gabrielrainajohnson@gmail.com>
2023-08-22 00:08:49 +0100

    gradient rainbow

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,11 @@ module.exports = {
         "dark-grey": "#757575",
         "opaque-black": "rgba(0, 0, 0, 36.5)",
       },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+      }),
     },
+    plugins: [],
   },
-  plugins: [],
 };
